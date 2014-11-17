1e4c3c1ec7e94ebbe74417286ed4efd60428c741
Rafael George <george.rafael@gmail.com>
2014-11-17 12:38:17 -0400

    Solved format words

--- format_words/format_words_spec.rb
+++ format_words/format_words_spec.rb
@@ -1,25 +1,38 @@
 require 'minitest/autorun'
 
-module FormatWords 
-  extend self 
+module FormatWords
+  extend self
 
   def call(words)
-    return [] if words.empty?
+    return '' if words.nil? || words.empty?
+    words.select! { |word| word =~ /\w+/ } if words.length > 1
+    return words.first if words.length == 1
     "#{words[0..-2].map { |w| w }.join(', ')} and #{words[-1]}"
   end
 end
 
-describe FormatWords do 
-
-  it 'returns a proper formatted word for an array of three words' do 
+describe FormatWords do
+  it 'returns a proper formatted word for an array of three words' do
     FormatWords.(['ninja', 'samurai', 'ronin']).must_equal 'ninja, samurai and ronin'
   end
 
-  it 'returns the same array for none words in it' do 
-    FormatWords.([]).must_equal []
+  it 'returns the same array for none words in it' do
+    FormatWords.([]).must_equal ''
   end
 
-  it 'returns a proper formatted word for two empty words and a word' do 
+  it 'returns a proper formatted word for two empty words and a word' do
     FormatWords.(['','','three']).must_equal 'three'
+  end
+
+  it 'returns the proper formatted word for one word' do
+    FormatWords.(['one']).must_equal 'one'
+  end
+
+  it 'returns "" for [""]' do
+    FormatWords.(['']).must_equal ''
+  end
+
+  it 'returns "" for nil' do
+    FormatWords.(nil).must_equal ''
   end
 end
