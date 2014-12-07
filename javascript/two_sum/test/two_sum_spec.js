c2714d5ccb4794ec0b8007c1ab3f7e111f990927
Rafael George <george.rafael@gmail.com>
2014-12-07 13:49:19 -0400

    Remove result and add two returns for simplifiying the code

--- javascript/two_sum/test/two_sum_spec.js
+++ javascript/two_sum/test/two_sum_spec.js
@@ -1,16 +1,14 @@
 var assert = require('assert');
 
 function twoSum(numbers, target) { 
-  var result = [];
   for (var idx = 0; idx < numbers.length; idx++) { 
     for (var jdx = idx+1; jdx < numbers.length; jdx++) { 
       if ((numbers[idx] + numbers[jdx]) === target) { 
-        result = [idx, jdx];
-        break;
+        return [idx, jdx];
       }
     }
   }
-  return result;
+  return [];
 }
 
 
