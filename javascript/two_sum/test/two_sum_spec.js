e3f311c0fb250b32d6a9e6e0be5299cc80ddfd55
Rafael George <george.rafael@gmail.com>
2014-12-07 13:43:30 -0400

    First expectation passing for TwoSums

--- javascript/two_sum/test/two_sum_spec.js
+++ javascript/two_sum/test/two_sum_spec.js
@@ -1,12 +1,26 @@
 var assert = require('assert');
 
 function twoSum(numbers, target) { 
-  return [];
+  var result = [];
+  for (var idx = 0; idx < numbers.length; idx++) { 
+    for (var jdx = idx+1; jdx < numbers.length; jdx++) { 
+      if ((numbers[idx] + numbers[jdx]) === target) { 
+        result = [idx, jdx];
+        break;
+      }
+    }
+  }
+  return result;
 }
+
+
 describe('TwoSum', function() { 
+  function numericalCompare(a,b) { 
+    return a - b;
+  }
+
   it('returns the indexes for the sum of 4', function() { 
-    assert.equal(twoSum([1,2,3],4).sort(function(a,b) { 
-      return a - b;
-    }), [0,2]);
+    assert.deepEqual(twoSum([1,2,3],4).sort(numericalCompare), [0,2]);
   })
 });
+
