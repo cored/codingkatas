8056fe0627fbe178d1a4c59941b2a6877b98cf98
Rafael George <george.rafael@gmail.com>
2014-12-07 13:45:52 -0400

    Add another assertion for twoSums with the same number in different indexes

--- javascript/two_sum/test/two_sum_spec.js
+++ javascript/two_sum/test/two_sum_spec.js
@@ -26,5 +26,9 @@ describe('TwoSum', function() {
   it('returns the indexes for the sum of 14690', function() { 
     assert.deepEqual(twoSum([1234,5678,9012],14690).sort(numericalCompare), [1,2]);
   })
+
+  it('returns the indexes for the sum of 4', function() { 
+    assert.deepEqual(twoSum([2,2,3],4).sort(numericalCompare), [0,1]);
+  })
 });
 
