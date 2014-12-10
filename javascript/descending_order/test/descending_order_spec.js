dcccdca891eebc1e501435e47f6552295ab3081d
Rafael George <george.rafael@gmail.com>
2014-12-10 12:51:59 -0400

    Javascript: Add Descending Order kata

---
+++ javascript/descending_order/test/descending_order_spec.js
@@ -0,0 +1,13 @@
+var assert = require("assert");
+
+function descendingOrder(number) { 
+  return 0;
+}
+
+describe("DescendingOrder", function() { 
+  describe("#descendingOrder", function() { 
+    it("returns 0 for 0", function() { 
+      assert.equal(descendingOrder(0), 0);
+    });
+  })
+});
