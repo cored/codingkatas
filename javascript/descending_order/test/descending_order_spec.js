27397629ba02fbb0903ca44a8cdb66f0e0a64c60
Rafael George <george.rafael@gmail.com>
2014-12-10 13:06:37 -0400

    Javascript: Add general solution for descending order kata

--- javascript/descending_order/test/descending_order_spec.js
+++ javascript/descending_order/test/descending_order_spec.js
@@ -1,13 +1,17 @@
 var assert = require("assert");
 
 function descendingOrder(number) { 
-  return 0;
+  return number.toString().split("").sort(function(a, b) { return b - a; }).join("");
 }
 
 describe("DescendingOrder", function() { 
   describe("#descendingOrder", function() { 
     it("returns 0 for 0", function() { 
-      assert.equal(descendingOrder(0), 0);
+      assert(descendingOrder(0), 0);
+    });
+
+    it("returns 987654321 for 123456789", function() { 
+      assert(descendingOrder(123456789), 987654321);
     });
   })
 });
