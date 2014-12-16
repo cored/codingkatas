7162b14729f0129df1aac664e61db0b59add912d
Rafael George <george.rafael@gmail.com>
2014-12-16 18:34:41 -0400

    Ruby: Solved Hashtag kata

--- ruby/hashtag/hashtag_spec.rb
+++ ruby/hashtag/hashtag_spec.rb
@@ -1,17 +1,47 @@
 require 'minitest/autorun'
 
-module Hashtag 
-  extend self 
+module Hashtag
+  extend self
 
   def call(post)
-    []
+    post.split(/\s/).
+      select { |w| w =~ /^#+\w+$/ }.
+      map { |w| w.gsub /#/, '' }
   end
 end
 
-describe Hashtag do 
+describe Hashtag do
+  it 'returns one item for "####many"' do
+    Hashtag.('####many').must_equal ['many']
+  end
 
-  it 'returns an empty array for empty string' do 
+  it 'returns an empty array for empty string' do
     Hashtag.('').must_equal []
   end
 
+  it 'returns an array with one word for #word' do
+    Hashtag.('#word').must_equal ['word']
+  end
+
+  it 'returns an empty array for string with ust hashtags' do
+    Hashtag.('# # # # #').must_equal []
+  end
+
+  it 'returns an array with the words with hashtags' do
+    Hashtag.('#hello #word #sorryNotSorry').must_equal %w|hello word sorryNotSorry|
+  end
+
+  it 'returns world for "# hello #world' do
+    Hashtag.("# hello #world").must_equal ["world"]
+  end
+
+  it 'returns empty array for "hash#line"' do
+    Hashtag.('hash#line').must_equal []
+  end
+
+  it 'returns empty for non alphabetic values' do
+    Hashtag.('#?blue').must_equal []
+    Hashtag.('#blue#blue').must_equal []
+    Hashtag.(' # blue').must_equal []
+  end
 end
