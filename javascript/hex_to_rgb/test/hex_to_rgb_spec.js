281e3e7570c0b8e32e76ec169468eb757ddaa10c
Rafael George <george.rafael@gmail.com>
2014-12-17 08:32:05 -0400

    Javascript: Solved HexToRgb

--- javascript/hex_to_rgb/test/hex_to_rgb_spec.js
+++ javascript/hex_to_rgb/test/hex_to_rgb_spec.js
@@ -1,5 +1,13 @@
 var expect = require("chai").expect
 
+var hexStringToRGB = function(hex) { 
+  var decimal_values = hex.match(/\w{1,2}/g).map(function(code) { 
+    return parseInt(code, 16);
+  })
+
+  return {r: decimal_values[0], g: decimal_values[1], b: decimal_values[2]};
+}
+
 describe("Convert a Hex String to RGB", function() { 
   it("colors for a passed hex code", function() { 
     expect(hexStringToRGB("#FF9933")).to.deep.equal({r:255, g:153, b:51});
