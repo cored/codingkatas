011a1251b55ffba43cafe0edbccf4c448c795278
Rafael George <george.rafael@gmail.com>
2014-12-17 19:05:48 -0400

    Ruby: Refactor ToCamelCase

--- ruby/to_camel_case/to_camel_case_spec.rb
+++ ruby/to_camel_case/to_camel_case_spec.rb
@@ -4,16 +4,14 @@ module ToCamelCase
   extend self
 
   def call(str)
-    return "" if str.nil? || str.empty?
-    pairs = str.split(/-|_/)
-    "#{pairs[0]}#{pairs[1..-1].map(&:capitalize).join}"
+    head, *tail = str.split(/[-_]/)
+    "#{head}#{tail.map(&:capitalize).join}"
   end
 end
 
 describe ToCamelCase do
   it 'returns blank for empty string' do 
     ToCamelCase.("").must_equal ""
-    ToCamelCase.(nil).must_equal ""
   end
 
   it 'returns camel case string with dash delimmiter' do
