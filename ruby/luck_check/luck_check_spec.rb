71e3e9d2a17fefc29d49dcc6588ed565f649047f
Rafael George <george.rafael@gmail.com>
2014-12-19 09:34:54 -0400

    Ruby: Solved Lucky Check

--- ruby/luck_check/luck_check_spec.rb
+++ ruby/luck_check/luck_check_spec.rb
@@ -1,30 +1,34 @@
-require 'minitest/autorun' 
+require 'minitest/autorun'
 
-module LuckCheck 
-  extend self 
+module LuckCheck
+  extend self
 
-  def call(str) 
-    return false if str.empty? || str =~ /[a-zA-Z]+/
+  def call(str)
+    raise if str =~ /[a-zA-Z]+/
+    return false if str.empty? || str.size % 2 != 0
+
     numbers = str.chars.map(&:to_i)
     numbers[0, numbers.size/2].reduce(:+) == numbers[numbers.size/2, numbers.size].reduce(:+)
   end
 end
 
-describe LuckCheck do 
-
-  it 'returns false for empty string' do 
+describe LuckCheck do
+  it 'returns false for empty string' do
     LuckCheck.("").must_equal false
   end
 
-  it 'returns false for non numeric string' do 
-    LuckCheck.("abc").must_equal false
+  it 'raise for non numeric string' do
+    ->{ LuckCheck.("abc") }.must_raise RuntimeError
   end
 
-  it 'returns false for non lucky numbers' do 
+  it 'returns false for non lucky numbers' do
     LuckCheck.("3344").must_equal false
+    LuckCheck.("111").must_equal false
   end
 
-  it 'returns true for a lucky number' do 
+  it 'returns true for a lucky number' do
     LuckCheck.("003111").must_equal true
+    LuckCheck.("1111").must_equal true
+    LuckCheck.("1111").must_equal true
   end
 end
