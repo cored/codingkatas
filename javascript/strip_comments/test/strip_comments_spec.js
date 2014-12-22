ca162a5a6c5867fcb22aacca38d7c7f13c2bbb6e
Rafael George <george.rafael@gmail.com>
2014-12-22 17:19:20 -0500

    Javascript: Strip comments solve case with two markers

--- javascript/strip_comments/test/strip_comments_spec.js
+++ javascript/strip_comments/test/strip_comments_spec.js
@@ -2,9 +2,10 @@ var expect = require("chai").expect
 
 var solution = function(str, markers) { 
   return markers.reduce(function(reduced, marker) { 
-    reduced.push(str.substring(0, str.indexOf(marker)));
+    str = str.substring(0, str.indexOf(marker));
+    reduced.push(str);
     return reduced;
-  }, []).join().trim();
+  }, []).join(" ").trim();
 }
 
 describe("Strip comments", function() { 
@@ -14,5 +15,9 @@ describe("Strip comments", function() {
 
   it("remove comments for two words", function() { 
     expect(solution("apples, pears #comment", ["#"])).to.equal("apples, pears");
+  });
+
+  it("use more than one comment marker", function() { 
+    expect(solution("apples, pears #comment !and here", ["#", "!"])).to.equal("apples, pears");
   });
 });
