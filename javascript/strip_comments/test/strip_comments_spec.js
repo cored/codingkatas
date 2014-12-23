787ac184a7cac826498fb5f9ca39ca898e608fc0
Rafael George <george.rafael@gmail.com>
2014-12-23 14:19:50 -0500

    Javascript: Solve StripComments

--- javascript/strip_comments/test/strip_comments_spec.js
+++ javascript/strip_comments/test/strip_comments_spec.js
@@ -1,23 +1,27 @@
-var expect = require("chai").expect 
+var expect = require("chai").expect
 
-var solution = function(str, markers) { 
-  return markers.reduce(function(reduced, marker) { 
-    str = str.substring(0, str.indexOf(marker));
-    reduced.push(str);
+var solution = function(str, markers) {
+  return str.split(new RegExp("["+markers.join("|")+"]s*.*")).reduce(function(reduced, phrase) { 
+    reduced.push(phrase.replace(/\s$/, ''));
     return reduced;
-  }, []).join(" ").trim();
+  }, []).join("");
 }
 
-describe("Strip comments", function() { 
-  it("remove comments for a single word", function() { 
+describe("Strip comments", function() {
+  it("remove comments for a single word", function() {
     expect(solution("apples #comment", ["#"])).to.equal("apples");
   });
 
-  it("remove comments for two words", function() { 
+  it("remove comments for two words", function() {
     expect(solution("apples, pears #comment", ["#"])).to.equal("apples, pears");
   });
 
-  it("use more than one comment marker", function() { 
+  it("use more than one comment marker", function() {
     expect(solution("apples, pears #comment !and here", ["#", "!"])).to.equal("apples, pears");
+  });
+
+  it("more than one marker and new lines", function() {
+    expect(solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])).
+      to.equal("apples, pears\ngrapes\nbananas");
   });
 });
