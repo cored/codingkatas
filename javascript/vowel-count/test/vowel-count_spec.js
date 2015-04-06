833eaafd8fa850dba186c85586aa2509dfae9557
cored <george.rafael@gmail.com>
2015-04-06 19:14:42 -0400

    Javascript: Add Vowel count kata

---
+++ javascript/vowel-count/test/vowel-count_spec.js
@@ -0,0 +1,28 @@
+var expect = require("chai").expect;
+
+var getCount = function(str) { 
+  var re = new RegExp("a|e|i|o|u", "g");
+  return str.match(re).length;
+}
+
+describe("Vowel count", function() { 
+  it("returns 1 for a", function() { 
+    expect(getCount("a")).to.equal(1);
+  })
+
+  it("returns 2 for ae", function() { 
+    expect(getCount("ae")).to.equal(2);
+  })
+
+  it("returns 3 for aei", function() { 
+    expect(getCount("aei")).to.equal(3);
+  })
+
+  it("returns 4 for aeio", function() { 
+    expect(getCount("aeio")).to.equal(4);
+  })
+
+  it("returns 5 for aeiou", function() { 
+    expect(getCount("aeiou")).to.equal(5);
+  })
+})
