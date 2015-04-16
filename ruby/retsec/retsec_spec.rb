85cfb6e20be20b8f5b18aa8bc78878e6ea5a355f
cored <george.rafael@gmail.com>
2015-04-16 11:38:26 -0400

    Ruby: Retsec add a new concept to the domain a Word

--- ruby/retsec/retsec_spec.rb
+++ ruby/retsec/retsec_spec.rb
@@ -4,8 +4,50 @@ module RetSec
   extend self 
 
   def call(word)
-    return "#{word[word.size/2..-1]}#{word[0...word.size/2]}" if word.size % 2 == 0
-    "#{word[(word.size/2+1)..-1]}#{word[word.size/2]}#{word[0..(word.size/2-1)]}"
+    word = Word.new(word)
+    return word.to_s
+  end
+
+  class Word 
+    def initialize(word)
+      @word = word
+    end
+
+    def to_s
+      "#{left}#{middle}#{right}"
+    end
+
+    private 
+
+    def left
+      if is_even?
+        word[word.size/2..-1]
+      else 
+        word[word[(word.size/2+1)..-1]]
+      end
+    end
+
+    def right 
+      if is_even?
+        word[0...word.size/2]
+      else 
+        word[0..(word.size/2-1)]
+      end
+    end
+
+    def middle 
+      if is_even?
+        ""
+      else 
+        word[word.size/2]
+      end
+    end
+
+    def is_even?
+      word.size % 2 == 0
+    end
+
+    attr_reader :word
   end
 end
 
